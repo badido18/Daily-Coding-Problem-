cbf237042013404776892b6e3a69d0859bf94ac9
badido18 <hm_boudis@esi.dz>
2020-04-17 21:43:33 +0100

    Google challenge 3 init"

---
+++ Google/challenge3.cpp
@@ -0,0 +1,54 @@
+/*
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Google.
+
+An XOR linked list is a more memory efficient doubly linked list.
+Instead of each node holding next and prev fields,
+it holds a field named both, which is an XOR of the next node and the previous node.
+Implement an XOR linked list; it has an add(element) which adds the element to the end,
+and a get(index) which returns the node at index.
+
+If using a language that has no pointers (such as Python),
+you can assume you have access to get_pointer and dereference_pointer functions
+that converts between nodes and memory addresses.
+*/
+
+// I used C++ fo easy pointers manipulation
+//implemetation of Xor liked list
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+struct {
+	int val ;
+	*Node npx ;
+}Node;
+
+Node Add(Node* list, int element) {
+	if node.npx == 0 {
+		Node n ;
+		n.val = element;
+		node->npx = getXor(0, &n):
+	}
+	else {
+		curr = node
+	}
+}
+
+*Node getXor(a *int, b *int){
+	return (a ^ b) ;
+}
+
+void  main() {
+	Node list ;
+	list.val = 5 ; 
+	list.npx = getXor(0,0); 
+	Add(list,6);
+	Add(list,5);
+	Add(list,9);
+	Add(list,1);
+	Add(list,3);
+	cout << Get(list,3)
+}
